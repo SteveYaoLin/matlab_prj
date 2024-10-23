15a0a2182c2b9ffac44a379df75815139716b5d3
姚琳 <yaolin0616@163.com>
2024-10-23 11:42:47 +0800

    横坐标显示为频率分辨率的倍数。s3在3415个点的位置。

--- sin1.m
+++ sin1.m
@@ -41,14 +41,18 @@ S3_mag(2:end-1) = 2*S3_mag(2:end-1); % 单边频谱加倍
 
 % 频率分辨率
 frequency_resolution = fs / N;    % 计算频率分辨率
-f_axis = (0:N/2) * frequency_resolution;  % 使用频率分辨率作为横轴单位
+f_axis = (0:N/2);                 % 使用索引作为横轴
 
 % 绘制频谱图
 figure;
-stem(f_axis/1e6, S3_mag, 'filled');  % 转换为 MHz 显示
-xlabel(['频率 (MHz), 分辨率 = ', num2str(frequency_resolution), ' Hz']);
+stem(f_axis, S3_mag, 'filled');  % 横坐标显示为索引
+xlabel(['频率索引, 每点对应的频率分辨率 = ', num2str(frequency_resolution), ' Hz']);
 ylabel('幅度');
 title('s3 波形的频谱图 (FFT)');
 
 % 显示 FFT 的频率分辨率
 disp(['FFT 频率分辨率: ', num2str(frequency_resolution), ' Hz']);
+
+% 计算目标频率在 FFT 中的索引
+s3_index = f3 / frequency_resolution;
+disp(['s3 的频率应在大约 ', num2str(s3_index), ' 点位置']);
